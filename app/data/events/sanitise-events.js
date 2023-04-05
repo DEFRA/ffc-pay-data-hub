73a80ac5a3945e3721969974aa1a82b70756565e
John Watson <john.watson2@rpa.gov.uk>
2023-04-05 11:28:51 +0100

    Split event name

--- app/data/events/sanitise-events.js
+++ app/data/events/sanitise-events.js
@@ -3,6 +3,7 @@ const { convertToPence } = require('../../currency-convert')
 const { PAYMENT_EXTRACTED } = require('../../constants/events')
 const schemeNames = require('../../constants/scheme-names')
 const { getStatus } = require('./get-event-status')
+const { getEventName } = require('./get-event-name')
 
 const sanitiseEvents = (events) => {
   return events.map(group => ({
@@ -19,64 +20,6 @@ const sanitiseEvents = (events) => {
       status: getEventName(event.type)
     }))
   }))
-}
-
-const getEventName = (type) => {
-  const eventMap = {
-    'uk.gov.defra.ffc.pay.payment.extracted': {
-      category: 'In progress',
-      detail: 'Extracted from batch'
-    },
-    'uk.gov.defra.ffc.pay.payment.enriched': {
-      category: 'In progress',
-      detail: 'Enriched'
-    },
-    'uk.gov.defra.ffc.pay.payment.paused.debt': {
-      category: 'Waiting',
-      detail: 'Waiting for debt data'
-    },
-    'uk.gov.defra.ffc.pay.payment.debt.attached': {
-      category: 'In progress',
-      detail: 'Debt data attached'
-    },
-    'uk.gov.defra.ffc.pay.payment.paused.ledger': {
-      category: 'Waiting',
-      detail: 'Ledger assignment'
-    },
-    'uk.gov.defra.ffc.pay.payment.ledger.assigned': {
-      category: 'In progress',
-      detail: 'Ledger assigned'
-    },
-    'uk.gov.defra.ffc.pay.payment.ledger.quality-check.pending': {
-      category: 'Waiting',
-      detail: 'Ledger assignment'
-    },
-    'uk.gov.defra.ffc.pay.payment.ledger.quality-check.failed': {
-      category: 'Waiting',
-      detail: 'Ledger correction'
-    },
-    'uk.gov.defra.ffc.pay.payment.ledger.quality-check.passed': {
-      category: 'In progress',
-      detail: 'Ledger confirmed'
-    },
-    'uk.gov.defra.ffc.pay.payment.processed': {
-      category: 'In progress',
-      detail: 'Final state calculated'
-    },
-    'uk.gov.defra.ffc.pay.payment.submitted': {
-      category: 'In progress',
-      detail: 'Submitted to D365'
-    },
-    'uk.gov.defra.ffc.pay.payment.acknowledged': {
-      category: 'Completed',
-      detail: 'Acknowledged by D365'
-    },
-    'uk.gov.defra.ffc.pay.payment.settled': {
-      category: 'Completed',
-      detail: 'Settled by D365'
-    }
-  }
-  return eventMap[type]
 }
 
 module.exports = {
