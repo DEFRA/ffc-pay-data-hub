0189ded71d6c33c068dab3cd0d6a0e566accb59e
John Watson <john.watson2@rpa.gov.uk>
2023-04-05 08:59:54 +0100

    Use categories

--- app/data/get-data.js
+++ app/data/get-data.js
@@ -1,11 +1,12 @@
+const { FRN, CORRELATION_ID } = require('../constants/categories')
 const { getEventsByFrn } = require('./events')
 const { getEventsByCorrelationId } = require('./events')
 
 const getData = async (category, value) => {
   switch (category) {
-    case 'frn':
+    case FRN:
       return await getEventsByFrn(value)
-    case 'correlationId':
+    case CORRELATION_ID:
       return await getEventsByCorrelationId(value)
     default:
       throw new Error(`Unknown category: ${category}`)
