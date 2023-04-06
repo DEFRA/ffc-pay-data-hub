2b67a522c381e326d005604372dde3d75108ba0b
John Watson <john.watson2@rpa.gov.uk>
2023-04-06 16:17:12 +0100

    Add events

--- app/data/events/sanitise-events.js
+++ app/data/events/sanitise-events.js
@@ -2,15 +2,14 @@ const moment = require('moment')
 const { convertToPence } = require('../../currency-convert')
 const { PAYMENT_EXTRACTED } = require('../../constants/events')
 const schemeNames = require('../../constants/scheme-names')
-const { getStatus } = require('./get-status')
-const { getEventName } = require('./get-event-name')
+const eventMap = require('./event-map')
 const { DATE } = require('../../constants/date-format')
 
 const sanitiseEvents = (events) => {
   return events.map(group => ({
     ...group,
     scheme: schemeNames[group.schemeId],
-    status: getStatus(group.events),
+    status: eventMap[group.events[group.events.length - 1].type],
     lastUpdated: moment(group.events[group.events.length - 1].time).format(DATE),
     events: group.events.map(event => ({
       ...event,
@@ -18,7 +17,7 @@ const sanitiseEvents = (events) => {
         ...event.data,
         value: event.type === PAYMENT_EXTRACTED ? convertToPence(event.data.value) : event.data.value
       },
-      status: getEventName(event.type),
+      status: eventMap[event.type],
       timestamp: moment(event.time).format(DATE)
     }))
   }))
