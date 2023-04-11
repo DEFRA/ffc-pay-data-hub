d55dd46805d7a96d711328dcc22c255b3994ec33
John Watson <john.watson2@rpa.gov.uk>
2023-04-11 16:43:53 +0100

    Add tests

--- app/cache/set-cached-response.js
+++ app/cache/set-cached-response.js
@@ -1,6 +1,6 @@
 const { get } = require('./get')
 const { update } = require('./update')
-const getRequestIndex = require('./get-request-index')
+const { getRequestIndex } = require('./get-request-index')
 
 const setCachedResponse = async (cacheName, key, request, response) => {
   const cacheData = await get(cacheName, key)
