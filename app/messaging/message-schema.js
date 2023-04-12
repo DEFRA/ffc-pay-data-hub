c2cb43171ebba3ac9489cf532ef3fc630623d466
John Watson <john.watson2@rpa.gov.uk>
2023-04-12 15:40:14 +0100

    Add tests

--- app/messaging/message-schema.js
+++ app/messaging/message-schema.js
@@ -1,8 +1,9 @@
 const Joi = require('joi')
+const { FRN, CORRELATION_ID } = require('../constants/categories')
 
 module.exports = Joi.object({
   body: Joi.object({
-    category: Joi.string().required(),
+    category: Joi.string().allow(FRN, CORRELATION_ID).required(),
     value: Joi.string().required()
   }).required()
 }).required()
---
+++ test/unit/messaging/message-schema.test.js
@@ -0,0 +1,34 @@
+const schema = require('../../../app/messaging/message-schema')
+const { REQUEST_MESSAGE } = require('../../mocks/messaging/message')
+
+let message
+
+describe('message schema', () => {
+  beforeEach(() => {
+    message = REQUEST_MESSAGE
+  })
+
+  test('should validate a valid message', () => {
+    expect(schema.validate(message).error).toBeUndefined()
+  })
+
+  test('should not validate a message with an invalid body', () => {
+    message.body = 'invalid'
+    expect(schema.validate(message).error).toBeDefined()
+  })
+
+  test('should not validate a message with a missing category', () => {
+    delete message.body.category
+    expect(schema.validate(message).error).toBeDefined()
+  })
+
+  test('should not validate a message with an invalid category', () => {
+    message.body.category = 'invalid'
+    expect(schema.validate(message).error).toBeDefined()
+  })
+
+  test('should not validate a message with a missing value', () => {
+    delete message.body.value
+    expect(schema.validate(message).error).toBeDefined()
+  })
+})
