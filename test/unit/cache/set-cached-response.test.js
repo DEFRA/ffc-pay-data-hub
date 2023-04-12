339fb325e82dea4ee0caf4693755aa557b310085
John Watson <john.watson2@rpa.gov.uk>
2023-04-12 12:02:53 +0100

    Add tests

---
+++ test/unit/cache/set-cached-response.test.js
@@ -0,0 +1,44 @@
+jest.mock('../../../app/cache/get')
+const { get: mockGet } = require('../../../app/cache/get')
+
+jest.mock('../../../app/cache/update')
+const { update: mockUpdate } = require('../../../app/cache/update')
+
+jest.mock('../../../app/cache/get-request-index')
+const { getRequestIndex: mockGetRequestIndex } = require('../../../app/cache/get-request-index')
+
+const { NAME } = require('../../mocks/cache/name')
+const { KEY } = require('../../mocks/cache/key')
+const { DATA } = require('../../mocks/cache/data')
+const { VALUE } = require('../../mocks/cache/value')
+
+const { setCachedResponse } = require('../../../app/cache/set-cached-response')
+
+describe('set cached response', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    mockGet.mockResolvedValue(DATA)
+    mockGetRequestIndex.mockReturnValue(0)
+  })
+
+  test('should get existing item from cache name and key', async () => {
+    await setCachedResponse(NAME, KEY, VALUE.request, VALUE.response)
+    expect(mockGet).toHaveBeenCalledWith(NAME, KEY)
+  })
+
+  test('should check if request already exists in cache', async () => {
+    await setCachedResponse(NAME, KEY, VALUE.request, VALUE.response)
+    expect(mockGetRequestIndex).toHaveBeenCalledWith(DATA, VALUE.request)
+  })
+
+  test('should update existing request in cache if already exists', async () => {
+    await setCachedResponse(NAME, KEY, VALUE.request, VALUE.response)
+    expect(mockUpdate).toHaveBeenCalledWith(NAME, KEY, DATA)
+  })
+
+  test('should add new request to cache if does not exist', async () => {
+    mockGetRequestIndex.mockReturnValue(-1)
+    await setCachedResponse(NAME, KEY, VALUE.request, VALUE.response)
+    expect(mockUpdate).toHaveBeenCalledWith(NAME, KEY, DATA)
+  })
+})
