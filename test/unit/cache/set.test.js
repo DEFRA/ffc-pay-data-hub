66e7a1dffa5613861ea1886318c238e8bdc48341
John Watson <john.watson2@rpa.gov.uk>
2023-04-14 15:34:39 +0100

    Feedback fixes

--- test/unit/cache/set.test.js
+++ test/unit/cache/set.test.js
@@ -13,7 +13,6 @@ const { PREFIX } = require('../../mocks/cache/prefix')
 const { NAME } = require('../../mocks/cache/name')
 const { KEY } = require('../../mocks/cache/key')
 const { VALUE, VALUE_STRING } = require('../../mocks/cache/value')
-const { cacheConfig } = require('../../../app/config')
 
 const { cacheConfig } = require('../../../app/config')
 
