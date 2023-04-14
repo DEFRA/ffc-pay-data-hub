ea46fc2847fafb12333b543339076d2c7a856034
John Watson <john.watson2@rpa.gov.uk>
2023-04-14 16:26:38 +0100

    Feedback fixes

--- app/data/events/correlation-id/get-events-by-correlation-id.js
+++ app/data/events/correlation-id/get-events-by-correlation-id.js
@@ -1,9 +1,9 @@
 const { CORRELATION_ID } = require('../../../constants/categories')
 const { getEvents } = require('../get-events')
-const { addPendingEvents } = require('../add-pending-events')
 const { orderGroupedEvents } = require('./order-grouped-events')
 const { groupEventsByCorrelationId } = require('./group-events-by-correlation-id')
 const { sanitiseEvents } = require('../sanitise-events')
+const { addPendingEvents } = require('../add-pending-events')
 
 const getEventsByCorrelationId = async (correlationId) => {
   const events = await getEvents(correlationId, CORRELATION_ID)
