b160cd0fae84ce730f07d13140933e80485bef17
Simon Dunn <simondunn90@gmail.com>
2023-05-24 09:57:20 +0100

    rename files

--- app/data/events/scheme-id/get-events-by-scheme.js
+++ app/data/events/scheme-id/get-events-by-scheme.js
@@ -4,7 +4,7 @@ const { groupEventsByScheme } = require('./group-events-by-scheme')
 const { orderGroupedEvents } = require('./order-grouped-events')
 const { sanitiseEvents } = require('../sanitise-events')
 
-const getEventsByScheme = async (schemeId) => {
+const getEventsBySchemeId = async (schemeId) => {
   const events = await getEvents(schemeId, SCHEME_ID) // filter to submitted events only
   const groupedEvents = groupEventsByScheme(events)
   // function to sum payments and calc total of payments.
@@ -13,5 +13,5 @@ const getEventsByScheme = async (schemeId) => {
 }
 
 module.exports = {
-  getEventsByScheme
+  getEventsBySchemeId
 }
