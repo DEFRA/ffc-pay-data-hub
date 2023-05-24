b0dc1ad52cc1a49f0aa0698e4d3b8cbc91c8c9b7
Simon Dunn <simondunn90@gmail.com>
2023-05-24 10:25:15 +0100

    only get events that have type of submitted

--- app/data/events/scheme-id/get-events-by-scheme.js
+++ app/data/events/scheme-id/get-events-by-scheme.js
@@ -1,11 +1,11 @@
 const { SCHEME_ID } = require('../../../constants/categories')
-const { getEvents } = require('../get-events')
 const { groupEventsByScheme } = require('./group-events-by-scheme')
 const { orderGroupedEvents } = require('./order-grouped-events')
 const { sanitiseEvents } = require('../sanitise-events')
+const { getSubmittedEvents } = require('./get-submitted-events')
 
 const getEventsBySchemeId = async (schemeId) => {
-  const events = await getEvents(schemeId, SCHEME_ID) // filter to submitted events only
+  const events = await getSubmittedEvents(schemeId, SCHEME_ID) // filter to submitted events only
   const groupedEvents = groupEventsByScheme(events)
   // function to sum payments and calc total of payments.
   const orderedEvents = orderGroupedEvents(groupedEvents) // change to sort by schemeId
--- app/data/events/scheme-id/get-submitted-events.js
+++ app/data/events/scheme-id/get-submitted-events.js
@@ -1,12 +1,10 @@
 const { getClient, odata } = require('../../../storage')
 const { PAYMENT_EVENT } = require('../../../constants/event-types')
-const submittedEvent = 'uk.gov.defra.ffc.pay.payment.submitted'
+const { PAYMENT_SUBMITTED } = require('../../../constants/events')
 
-// only get the events that have type of submitted
-// amend this logic or create new function as this is reused by multiple
 const getSubmittedEvents = async (partitionKey, category) => {
   const client = getClient(PAYMENT_EVENT)
-  const eventResults = client.listEntities({ queryOptions: { filter: odata`PartitionKey eq ${partitionKey.toString()} and category eq '${category}' and type eq '${submittedEvent}'` } })
+  const eventResults = client.listEntities({ queryOptions: { filter: odata`PartitionKey eq ${partitionKey.toString()} and category eq '${category}' and type eq '${PAYMENT_SUBMITTED}'` } })
   const events = []
   for await (const event of eventResults) {
     event.data = JSON.parse(event.data)
