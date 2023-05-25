ec10e830c3765a2a97d3986b6b7642aa5a06fef3
Simon Dunn <simondunn90@gmail.com>
2023-05-25 13:51:15 +0100

    tidy up

--- app/data/events/scheme-id/get-events-by-scheme.js
+++ app/data/events/scheme-id/get-events-by-scheme.js
@@ -6,10 +6,10 @@ const { orderGroupedEventsByScheme } = require('./order-grouped-events-by-scheme
 const { sanitiseSchemeData } = require('./sanitise-scheme-data')
 
 const getEventsBySchemeId = async (schemeId) => {
-  const events = await getSubmittedEvents(schemeId, SCHEME_ID) // filter to submitted events only
+  const events = await getSubmittedEvents(schemeId, SCHEME_ID)
   const groupedEvents = groupEventsByScheme(events)
   const totalSchemeValues = getTotalSchemeValues(groupedEvents)
-  const orderedSchemeData = orderGroupedEventsByScheme(totalSchemeValues) // change to sort by schemeId
+  const orderedSchemeData = orderGroupedEventsByScheme(totalSchemeValues)
   return sanitiseSchemeData(orderedSchemeData)
 }
 
--- app/data/events/scheme-id/sanitise-scheme-data.js
+++ app/data/events/scheme-id/sanitise-scheme-data.js
@@ -3,8 +3,8 @@ const { convertToPounds } = require('../../../currency-convert')
 // this makes the data easier for payweb to consume
 const sanitiseSchemeData = (schemeData) => {
   return schemeData.map(scheme => ({
-    ...scheme,
     scheme: schemeNames[scheme.schemeId],
+    paymentRequests: scheme.paymentRequests,
     value: convertToPounds(scheme.value)
   }))
 }
