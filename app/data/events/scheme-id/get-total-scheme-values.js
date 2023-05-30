37f507493996e77152585bb11b4a1b563a14a2a5
Simon Dunn <simondunn90@gmail.com>
2023-05-30 10:19:46 +0100

    tidy

--- app/data/events/scheme-id/get-total-scheme-values.js
+++ app/data/events/scheme-id/get-total-scheme-values.js
@@ -1,5 +1,4 @@
 const getTotalSchemeValues = (groupedEvents) => {
-  // loop over grouped events and sum all values and return schemeId, total number of payments, total value
   return groupedEvents.map((eventsByScheme) => {
     const value = eventsByScheme.events.reduce((total, current) => {
       total += current.data.value
