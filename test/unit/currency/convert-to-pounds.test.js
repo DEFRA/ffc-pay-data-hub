ca958ec6f4322fc1a8d4a0001243922cd628daf6
John Watson <john.watson1@defra.gov.uk>
2023-05-30 14:37:40 +0100

    Feedback

--- test/unit/currency/convert-to-pounds.test.js
+++ test/unit/currency/convert-to-pounds.test.js
@@ -6,9 +6,9 @@ describe('convert to pounds', () => {
     expect(result).toEqual('1.00')
   })
 
-  test('converts 100 to pounds with no decimals', () => {
-    const result = convertToPounds(100.00)
-    expect(result).toEqual('1.00')
+  test('converts 110 to pounds', () => {
+    const result = convertToPounds(110)
+    expect(result).toEqual('1.10')
   })
 
   test('converts -100 to pounds', () => {
@@ -16,13 +16,18 @@ describe('convert to pounds', () => {
     expect(result).toEqual('-1.00')
   })
 
+  test('converts 100 to pounds with no decimals', () => {
+    const result = convertToPounds(100.00)
+    expect(result).toEqual('1.00')
+  })
+
   test('converts 100.10 to pounds ignoring decimal', () => {
     const result = convertToPounds(100.10)
     expect(result).toEqual('1.00')
   })
 
-  test('converts 110 to pounds', () => {
-    const result = convertToPounds(110)
-    expect(result).toEqual('1.10')
+  test('converts -100.10 to pounds ignoring decimal', () => {
+    const result = convertToPounds(-100.10)
+    expect(result).toEqual('-1.00')
   })
 })
