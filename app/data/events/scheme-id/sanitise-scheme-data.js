468ebdc1d070f1be66f00da0092e11f4deda1137
Simon Dunn <simondunn90@gmail.com>
2023-05-31 08:28:11 +0100

    remove comment

--- app/data/events/scheme-id/sanitise-scheme-data.js
+++ app/data/events/scheme-id/sanitise-scheme-data.js
@@ -1,6 +1,6 @@
 const schemeNames = require('../../../constants/scheme-names')
 const { convertToPounds } = require('../../../currency')
-// this makes the data easier for payweb to consume
+
 const sanitiseSchemeData = (schemeData) => {
   return schemeData.map(scheme => ({
     scheme: schemeNames[scheme.schemeId],
