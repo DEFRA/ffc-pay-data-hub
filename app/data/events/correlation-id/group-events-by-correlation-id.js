2e10f25d7b42b8b6233c011f393b07dffc8a31e2
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-05-17 10:16:10 +0100

    refactor nesting and object assign to spread syntax

--- app/data/events/correlation-id/group-events-by-correlation-id.js
+++ app/data/events/correlation-id/group-events-by-correlation-id.js
@@ -2,15 +2,17 @@ const groupEventsByCorrelationId = (events) => {
   return [...events.reduce((x, y) => {
     const key = y.correlationId
 
-    const item = x.get(key) || Object.assign({}, {
-      frn: y.data.frn,
-      correlationId: y.partitionKey,
-      schemeId: y.data.schemeId,
-      paymentRequestNumber: y.data.paymentRequestNumber,
-      agreementNumber: y.data.agreementNumber,
-      marketingYear: y.data.marketingYear,
-      events: []
-    })
+    const item = x.get(key) || {
+      ...{
+        frn: y.data.frn,
+        correlationId: y.partitionKey,
+        schemeId: y.data.schemeId,
+        paymentRequestNumber: y.data.paymentRequestNumber,
+        agreementNumber: y.data.agreementNumber,
+        marketingYear: y.data.marketingYear,
+        events: []
+      }
+    }
     item.events.push(y)
 
     return x.set(key, item)
--- app/data/events/frn/group-events-by-frn.js
+++ app/data/events/frn/group-events-by-frn.js
@@ -3,15 +3,17 @@ const groupEventsByFrn = (events) => {
     const correlationId = y.rowKey.split('|')[0]
     const key = `${y.partitionKey}-${correlationId}`
 
-    const item = x.get(key) || Object.assign({}, {
-      frn: y.partitionKey,
-      correlationId,
-      schemeId: y.data.schemeId,
-      paymentRequestNumber: y.data.paymentRequestNumber,
-      agreementNumber: y.data.agreementNumber,
-      marketingYear: y.data.marketingYear,
-      events: []
-    })
+    const item = x.get(key) || {
+      ...{
+        frn: y.partitionKey,
+        correlationId,
+        schemeId: y.data.schemeId,
+        paymentRequestNumber: y.data.paymentRequestNumber,
+        agreementNumber: y.data.agreementNumber,
+        marketingYear: y.data.marketingYear,
+        events: []
+      }
+    }
     item.events.push(y)
 
     return x.set(key, item)
