31e999c1d1cc4b38384b60945366b1f709db9188
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2025-09-29 17:00:35 +0100

    for FC only, use second event value if first event has none (#52)

--- test/unit/data/events/add-values.test.js
+++ test/unit/data/events/add-values.test.js
@@ -7,6 +7,7 @@ const processed = require('../../../mocks/events/processed')
 const submitted = require('../../../mocks/events/submitted')
 
 const { addValues } = require('../../../../app/data/events/add-values')
+const { FC } = require('../../../../app/constants/schemes')
 
 let groupedEvent
 
@@ -14,7 +15,7 @@ describe('add value to events', () => {
   beforeEach(() => {
     groupedEvent = JSON.parse(JSON.stringify(require('../../../mocks/events/grouped-event')))
     groupedEvent.events = [enriched, processed, submitted, acknowledged]
-    mockConvertToString.mockImplementation(value => value.toString())
+    mockConvertToString.mockImplementation(value => value ? value.toString() : 0)
   })
 
   test('should return empty array if no events', () => {
@@ -45,5 +46,20 @@ describe('add value to events', () => {
   test('should add current value formatted as string', () => {
     const result = addValues([groupedEvent])
     expect(result[0].currentValueText).toBe(acknowledged.data.value.toString())
+  })
+
+  test('should add original value from second event if first is absent for FC', () => {
+    delete groupedEvent.events[0].data.value
+    groupedEvent.events[0].data.schemeId = FC
+    const result = addValues([groupedEvent])
+    expect(result[0].originalValue).toBe(processed.data.value)
+  })
+
+  test('should add original value formatted as string from second event if first is absent for FC', () => {
+    delete groupedEvent.events[0].data.value
+    groupedEvent.events[0].data.schemeId = FC
+    groupedEvent.events[1].data.value = 999
+    const result = addValues([groupedEvent])
+    expect(result[0].originalValueText).toBe(processed.data.value.toString())
   })
 })
